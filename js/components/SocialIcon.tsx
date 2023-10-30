d566c91f196044dfa0b0912832d5731c8267e002
Jonetta <117986629+JonettaPek@users.noreply.github.com>
2023-10-30 10:10:45 +0000

    reformat code

--- js/components/SocialIcon.tsx
+++ js/components/SocialIcon.tsx
@@ -1,10 +1,10 @@
 const SocialIcon = function ({url, alt, logoSource}) {
     return `
-    <li class="footer__item">
-        <a href="${url}" target="_blank">
-            <img src="${logoSource}" alt="${alt}'>
-        </a>
-    </li>
+        <li class="footer__item">
+            <a href="${url}" target="_blank">
+                <img src="${logoSource}" alt="${alt}'>
+            </a>
+        </li>
     `;
 };
 
