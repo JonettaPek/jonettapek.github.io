f5985891f67d1303556f25295b9ebb1a870668bb
Jonetta <117986629+JonettaPek@users.noreply.github.com>
2023-10-30 10:44:36 +0000

    refactor runtime rendering of social icons to components

--- js/script.js
+++ js/script.js
@@ -1,17 +1,8 @@
 import socials from "./data/socials.js"
+import SocialsList from "./components/SocialsList.js";
 
 document.querySelector(".header__blog-name").innerHTML = "One T for Thoughts <br> with J { one } tta";
 
-socials.forEach((social) => {
-    let li = document.createElement("li");
-    li.setAttribute("class", "footer__item");
-    let a = document.createElement("a");
-    a.setAttribute("href", social.url);
-    a.setAttribute("target", "_blank");
-    let img = document.createElement("img");
-    img.setAttribute("src", social.logoSource);
-    img.setAttribute("alt", social.alt);
-    a.append(img);
-    li.append(a);
-    document.querySelector("body footer ul").append(li);
-});
+document.querySelector(".footer").innerHTML = SocialsList(socials);
+
+console.log(SocialsList(socials));
