e97a9c0876653faba264fc57527b7202de4d6cc9
Jonetta <117986629+JonettaPek@users.noreply.github.com>
2023-10-30 08:52:42 +0000

    modify socials object to a list

--- js/data/socials.js
+++ js/data/socials.js
@@ -1,19 +1,19 @@
-const socials = {
-    LinkedIn: {
+const socials = [
+    {
         url: "https://www.linkedin.com/in/jonetta/",
         alt: "LinkedIn",
         logoSource: "https://img.shields.io/badge/LinkedIn-%230A66C2?style=for-the-badge&logo=LinkedIn"
     },
-    GitHub: {
+    {
         url: "https://github.com/JonettaPek",
         alt: "GitHub",
         logoSource: "https://img.shields.io/badge/GitHub-%23181717?style=for-the-badge&logo=GitHub"
     },
-    YouTube: {
+    {
         url: "",
         alt: "YouTube",
         logoSource: "https://img.shields.io/badge/YouTube-%23FF0000?style=for-the-badge&logo=YouTube"
     },
-}
+]
 
 export default socials;
